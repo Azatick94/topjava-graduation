975d151b6b6ae68baeceb660828f0f6c3ea04968
Azatick94 <azburhanov@mail.ru>
2021-03-22 17:35:34 +0300

    small-fix

--- src/main/java/com/graduation/web/HelloWorld.java
+++ src/main/java/com/graduation/web/HelloWorld.java
@@ -1,14 +1,12 @@
 package com.graduation.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("/")
+@RestController("/")
 public class HelloWorld {
 
     @GetMapping("/")
-    @ResponseBody
     public String helloWorld() {
         return "Hello World";
     }
