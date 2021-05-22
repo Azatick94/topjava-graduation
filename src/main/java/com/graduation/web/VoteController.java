16a460256c93ff074142457383e93914d27da511
Azatick94 <azburhanov@mail.ru>
2021-05-22 12:46:32 +0300

    removed delete vote from tests

--- src/main/java/com/graduation/web/VoteController.java
+++ src/main/java/com/graduation/web/VoteController.java
@@ -15,7 +15,6 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/test/java/com/graduation/web/VoteControllerTest.java
+++ src/test/java/com/graduation/web/VoteControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,30 +22,4 @@ class VoteControllerTest extends AbstractMockMvcTest {
                 .andExpect(content().string(
                         containsString("\"id\":100034,\"userId\":100001,\"restaurantId\":100008")));
     }
-
-    /**
-     * Delete Vote as Admin, Should be Successful result
-     */
-    @WithMockUser(username = SECURITY_ADMIN_LOGIN, roles = SECURITY_ADMIN_ROLE)
-    @Test
-    void deleteAdminTest() throws Exception {
-        this.mockMvc.perform(delete(VOTE_REST + "/100035"));
-
-        this.mockMvc.perform(get(VOTE_REST + "/100035"))
-                .andDo(print())
-                .andExpect(status().isNotFound());
-    }
-
-    /**
-     * Delete Vote as Admin, Should be Security Exception
-     */
-    @WithMockUser(username = SECURITY_USER_LOGIN, roles = SECURITY_USER_ROLE)
-    @Test
-    void deleteUserTest() throws Exception {
-        this.mockMvc
-                .perform(delete(VOTE_REST + "/100035"))
-                .andExpect(status().is4xxClientError());
-    }
-
-
 }
