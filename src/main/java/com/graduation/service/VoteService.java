f1615564d315fe96b96aac5986297203de1594f0
Azatick94 <azburhanov@mail.ru>
2021-05-23 19:31:30 +0300

    small fix

--- src/main/java/com/graduation/service/VoteService.java
+++ src/main/java/com/graduation/service/VoteService.java
@@ -10,7 +10,6 @@ import com.graduation.to.VoteTo;
 import com.graduation.util.exception.CustomMessageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
